c52eb83508303e741536edb3f0eafa6f55ed8485
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-05-13 21:23:41 +0300

    jinja2 #2

--- jinja_practic.py
+++ jinja_practic.py
@@ -83,3 +83,101 @@
 # msg = tm.render(p = per)
 #
 # print(msg)
+
+
+# from jinja2 import Template
+#
+# data = """ {% raw %}Какой-то текст, который не нужно преобразовывать {{ name }} {% endraw %}"""
+# name = "Заяц"
+#
+# tm = Template(data)
+# msg = tm.render(name= name)
+#
+# print(msg)
+
+
+# from jinja2 import Template
+#
+# data = """ В HTML документе ссылки определяются так:
+#     <a href='#'>link</a>"""
+#
+# tm = Template("{{ data | e}}")
+# msg = tm.render(data=data)
+#
+# print(msg)
+
+
+#
+# from markupsafe import escape
+#
+# data = """ В HTML документе ссылки определяются так:
+#     <a href='#'>link</a>"""
+#
+# msg = escape(data)
+#
+# print(msg)
+
+
+# from jinja2 import Template
+#
+# cities = [{'id' : 1, 'city' : 'Moscow'},
+#           {'id' : 2, 'city' : 'Paris'},
+#           {'id' : 3, 'city' : 'London'}
+#           ]
+#
+# link = '''<select name="cities">
+# {% for c in cities %}
+#     <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+# {% endfor %}
+# </select>'''
+#
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
+
+#
+# from jinja2 import Template
+#
+# cities = [{'id' : 1, 'city' : 'Moscow'},
+#           {'id' : 2, 'city' : 'Paris'},
+#           {'id' : 3, 'city' : 'London'}
+#           ]
+#
+# link = '''<select name="cities">
+# {% for c in cities -%}
+#     <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+# {% endfor -%}
+# </select>'''
+#
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
+
+
+
+# from jinja2 import Template
+#
+# cities = [{'id' : 1, 'city' : 'Moscow'},
+#           {'id' : 2, 'city' : 'Paris'},
+#           {'id' : 3, 'city' : 'London'},
+#           {'id' : 4, 'city' : 'Orel'}
+#           ]
+#
+# link = '''<select name="cities">
+# {% for c in cities -%}
+# {% if c.id > 2 -%}
+#     <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+# {% elif c.city == 'Moscow' -%}
+#     <option>{{ c['city'] }}</option>
+# {% else -%}
+#     {{ c['city'] }}
+# {% endif -%}
+# {% endfor -%}
+# </select>'''
+#
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
